fa3b2caa5e1b35320608a7d90ec43f0255fb6db1
Ahmed Ali <ahmed_Sameh_2020@hotmail.com>
2020-06-20 03:25:09 +0200

    adding api unit test

---
+++ Decision.WebApiTests/Controllers/QuestionnaireControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Decision.WebApi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Decision.Services;
+
+namespace Decision.WebApi.Controllers.Tests
+{
+    [TestClass()]
+    public class QuestionnaireControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            var result = new QuestionnaireController(new QuestionService()).Get();
+            Assert.IsNotNull(result);
+        }
+    }
+}
